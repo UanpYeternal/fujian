0facd0a133910c68bd20802d59eecdeabcff899f
DerryCross <derrycross@gmail.com>
2013-12-06 20:21:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fujian::Application.config.secret_key_base = 'e0c1256ad8c59ce39a8024f512b4e26de3781f139d29ffea738fb2934993674877bf8e08c77e69e4b03afc710ab6784653d5826aef0387b6365af6d7e474eb29'
